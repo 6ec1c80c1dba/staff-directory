fe392928da3399c20a7f22c4aedb922809bdf8ae
6ec1c80c1dba <hollyblondon2020@gmail.com>
2023-03-26 12:06:11 +0100

    lifetime

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,12 +5,14 @@ from flask import Flask, Response
 from flask_talisman import Talisman
 from flask_marshmallow import Marshmallow
 from flask_wtf.csrf import CSRFProtect, CSRFError
+from datetime import timedelta
 
 def create_app(test_config=None):
     """Creation and configuration of the application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = "2-sIth,P64bynlE/=X6va~,K_woB[z"
     app.config['DATABASE']= os.path.join(app.instance_path, 'flaskr.sqlite')
+    app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=5)
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -41,9 +43,9 @@ def create_app(test_config=None):
     else:
         CSRFProtect(app)
 
-    # @app.errorhandler(CSRFError)
-    # def handle_csrf_error(e):
-    #     return 'CSRF error - user forbidden', 403
+    @app.errorhandler(CSRFError)
+    def handle_csrf_error(e):
+        return 'CSRF error - user forbidden', 403
 
     from . import db
     db.init_app(app)
